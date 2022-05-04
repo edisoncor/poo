a36b7cc1acd63bbcfb176f8d69da2e4a6685b057
edisoncor <edisoncor@gmail.com>
2022-05-04 13:18:17 -0500

    Ejejmplo 2
    Ejejmplos en java

---
+++ ClinicaVeterinaria/src/clinicaveterinaria/Raza.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinicaveterinaria;
+
+/**
+ *
+ * @author edisoncor
+ */
+public class Raza {
+    
+    public String tamanio;
+    public int esperanzaDeVida;
+    public String nombre;
+    
+    
+    public Animal animal;
+
+    public Raza(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Raza() {
+    }
+    
+    
+    
+    
+}
