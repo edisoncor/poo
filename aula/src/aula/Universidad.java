dd19a3510d83d6abe8ccf7f5396db08f618aaf50
Edison Coronel <edisoncor@gmail.com>
2022-05-24 15:49:14 -0500

    ejemplo de java

---
+++ aula/src/aula/Universidad.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula;
+
+/**
+ *
+ * @author edisoncor
+ */
+public class Universidad {
+ 
+    private String nombre;
+    private String siglas;
+    
+    private Facultad[] facultadList;
+
+    public Universidad() {
+        this.facultadList = new Facultad[5];
+    }
+
+    public Universidad(String nombre) {
+        this();
+        this.nombre = nombre;
+    }
+    
+    public Universidad(String nombre, String siglas){
+        this();
+        this.nombre = nombre;
+        this.siglas = siglas;
+    }
+      
+    @Override
+    public String toString(){
+        return "" + this.nombre + " - " + this.siglas;
+    }
+    
+    
+}
