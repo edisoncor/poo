0005223ee16aa83a94ebae63463878b6fecc8936
edisoncor <edisoncor@gmail.com>
2022-05-27 10:28:53 -0500

    Lista enlazada
    ejemplo de lista enlazada

---
+++ ListaEnlazada/src/listaenlazada/Nodo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaenlazada;
+
+/**
+ *
+ * @author edisoncor
+ */
+public class Nodo {
+    
+    public int dato;
+    public Nodo siguiente = null;
+    
+    public Nodo(int numero){
+        this.dato = numero;
+    }
+
+    @Override
+    public String toString() {
+        return " " + dato + ' ';
+    }
+    
+    
+    
+}
